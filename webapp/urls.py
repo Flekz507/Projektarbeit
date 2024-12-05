783c5951b8d25ea15040f426abea38d8536ab60d
Flekz507 <151685398+Flekz507@users.noreply.github.com>
2024-12-05 16:59:04 +0100

    Add files via upload

---
+++ webapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('nails4you.urls')),
+    path('cart/', include('cart.urls')),
+    path('payment/', include('payment.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
